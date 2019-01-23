9e858636b45ac29ed6aeb4cbdad908213c17b231
NomanGul <nomangul2001@gmail.com>
2019-01-23 10:09:45 +0500

    Updates

---
+++ precache-manifest.5657724b3333dd8a6f6f971989956286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94ad6ef5ed507b8f9e8a",
+    "url": "/fazail-e-amaal/static/css/main.0b197968.chunk.css"
+  },
+  {
+    "revision": "94ad6ef5ed507b8f9e8a",
+    "url": "/fazail-e-amaal/static/js/main.94ad6ef5.chunk.js"
+  },
+  {
+    "revision": "e75614a9f374b190fd91",
+    "url": "/fazail-e-amaal/static/js/1.e75614a9.chunk.js"
+  },
+  {
+    "revision": "5e75b1e63ad3407894aa",
+    "url": "/fazail-e-amaal/static/js/runtime~main.5e75b1e6.js"
+  },
+  {
+    "revision": "6e3895abe2f4d84e314dcf353161efbb",
+    "url": "/fazail-e-amaal/static/media/FAZAIL_E_AAMAAL.6e3895ab.pdf"
+  },
+  {
+    "revision": "a6fae24292274e4bb6fd22d9aa13c310",
+    "url": "/fazail-e-amaal/index.html"
+  }
+];
